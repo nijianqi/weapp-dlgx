b21c5c28970bb330a776c9589ffc8b25042e98b8
nijianqi <1198952901@qq.com>
2017-01-11 00:34:44 +0800

    首页-style
    首页-style

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,9 +3,14 @@
 var app = getApp()
 Page({
   data: {
-    bannerList: [],
+    inputShowed: false,
+    inputVal: "",
+    bannerList: [], //首页轮播图
+    activityOnlineList:[],
+    activityLineList:[],
+    videoList:[]
   },
-  //首页轮播图
+  //首页
   onLoad:function(options){
   var that = this;
   wx.request({
@@ -17,9 +22,33 @@ Page({
   success: function(res) {
    var data = res.data;
    that.setData({
-        bannerList: data.bannerList
+        bannerList: data.bannerList,
+        activityOnlineList: data.activityOnlineList.slice(0, 4),
+        activityLineList: data.activityLineList.slice(0, 3),
+        videoList: data.videoList
       })
   }
 })
-  }
+  },
+   showInput: function () {
+        this.setData({
+            inputShowed: true
+        });
+    },
+    hideInput: function () {
+        this.setData({
+            inputVal: "",
+            inputShowed: false
+        });
+    },
+    clearInput: function () {
+        this.setData({
+            inputVal: ""
+        });
+    },
+    inputTyping: function (e) {
+        this.setData({
+            inputVal: e.detail.value
+        });
+    }
 })
